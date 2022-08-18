eae08eb46ce0185b99b6e1e724c63d838fd1c653
tengy <tengy300@yahoo.com>
2022-08-18 12:22:54 -0500

    added some notes

---
+++ tengy.js
@@ -0,0 +1,6 @@
+enketi@liontechnologysolutions.com●	
+Have experience with SonarQube for code quality analysis using defined benchmarks. 
+Use Nexus to store artifacts in repositories. 
+Install and configure web application Servers (JBoss/Wildfly, Nginx, apache, Tomcat)
+Used Jenkins for CI/CD framework in a Linux environment
+
