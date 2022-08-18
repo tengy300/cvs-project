2d00ef23b8f1c1cfaf9a6298046ebd0e21b4c8b6
tengy300 <101148367+tengy300@users.noreply.github.com>
2022-08-18 12:34:14 -0500

    Update tengy.js

--- tengy.js
+++ tengy.js
@@ -3,4 +3,4 @@ Have experience with SonarQube for code quality analysis using defined benchmark
 Use Nexus to store artifacts in repositories. 
 Install and configure web application Servers (JBoss/Wildfly, Nginx, apache, Tomcat)
 Used Jenkins for CI/CD framework in a Linux environment
-
+God is in Control. With God and myself, ALl THINGS ARE POSSIBLE FOR ME.
